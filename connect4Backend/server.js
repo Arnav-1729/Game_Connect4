286fb07c084be4c55dadfede7841b8344e78455a
Arnav Talwar <77537880+Arnav-1729@users.noreply.github.com>
2024-03-28 17:53:58 +0530

    Add files via upload

---
+++ connect4Backend/server.js
@@ -0,0 +1,219 @@
+require('dotenv').config()
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const app = express();
+const server = createServer(app);
+const path= require("path");
+
+app.use(express.static(path.join(__dirname,"build")));
+
+const io = new Server(server, {
+  cors: {
+    // origin: process.env.FRONTEND_SOCKET,
+    methods: ["GET", "POST"],
+    transports: ['websocket', 'polling'],
+    credentials: true,
+  },
+  allowEIO3: true
+});
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND,
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   })
+// );
+const rooms = {};
+const lobby = [];
+io.on("connection", (socket) => {
+  // console.log("user connected on socket ID:  ", socket.id);
+  socket.on("createRoom", ({ roomName, password, nickName }) => {
+    // console.log("hello from create room");
+    if (rooms[roomName]) {
+      socket.emit("invalidRoom", { message: "Room already exists" });
+    } else {
+      rooms[roomName] = { password, players: [] };
+      rooms[roomName].players.push({
+        id: socket.id,
+        color:  "red",
+        nickName: nickName,
+      });
+      socket.join(roomName);
+      socket.emit("roomJoined", { rooms, roomName, playerColor: "red", playerCount:1 });
+    }
+  });
+  socket.on("joinRoom", ({ roomName, password, nickName }) => {
+    const room = rooms[roomName];
+    // console.log("hello from join room", room);
+    if (room && room.password === password && room.players.length < 2) {
+      const playerColor = room.players.length === 0 ? 'red' : (room.players[0].color === 'red' ? 'blue' : 'red');
+      oppositePlayer=room.players[0];
+      rooms[roomName].players.push({
+        id: socket.id,
+        color: playerColor,
+        nickName: nickName,
+      });
+      socket.join(roomName);
+      socket.emit("roomJoined", {
+        playerCount: rooms[roomName].players.length, rooms, roomName, playerColor,
+      });
+      socket.emit("playerJoined", {
+        oppositePlayer:oppositePlayer.nickName,
+      });
+      io.to(oppositePlayer.id).emit("playerJoined", {
+        oppositePlayer:nickName,
+      });
+      if (room.players.length === 2) {
+        io.to(roomName).emit("startGame");
+      }
+    } else {
+      socket.emit("invalidRoom");
+    }
+  });
+  socket.on("disconnect", () => {
+    for (const roomName in rooms) {
+      // console.log(roomName);
+      const index = rooms[roomName].players.findIndex(
+        (player) => player.id === socket.id
+      );
+      if (index !== -1) {
+        rooms[roomName].players.splice(index, 1);
+        io.to(roomName).emit("playerLeft", {
+          playerCount: rooms[roomName].players.length,
+        });
+        break;
+      }
+    }
+  });
+  socket.on("updateGame", ({ roomName, newGrid, currentTurn, col }) => {
+    // console.log("hello from updateGame", roomName, currentTurn, newGrid);
+    const newTurn = currentTurn === "red" ? "blue" : "red";
+    // console.log(checkWinner(newGrid, col));
+    io.to(roomName).emit("gameState", {
+      newGrid,
+      newTurn,
+      newWinner: checkWinner(newGrid, col),
+    });
+  });
+  socket.on("restartGame", ({roomName, newGrid})=>{
+    // console.log("hello from restartGame");
+    io.to(roomName).emit("gameState", {newGrid, newTurn:"red", newWinner:null});
+  });
+  io.engine.on("connection_error", (err) => {
+    console.log(err.req); 
+    console.log(err.code);
+    console.log(err.message);
+    console.log(err.context);
+  });
+  socket.on('enterLobby', ({nickName}) => {
+    lobby.push({socket, nickName});
+    // console.log("hello from enterlobby ", nickName)
+    tryMatchPlayers();
+  });
+  socket.on('leaveLobby', () => {
+    const index = lobby.indexOf(socket.id);
+    if (index !== -1) {
+      lobby.splice(index, 1);
+    }
+  });
+  function tryMatchPlayers() {
+    if (lobby.length >= 2) {
+      const player1 = lobby.shift();
+      const player2 = lobby.shift();
+      const roomName = `${player1.socket.id}-${player2.socket.id}`;
+      // console.log("roomName from match player", roomName);
+      rooms[roomName] = {
+        players: [],
+      };
+      rooms[roomName].players.push({
+        id: player1.socket.id,
+        color:  "red",
+      });
+      rooms[roomName].players.push({
+        id: player2.socket.id,
+        color:  "blue",
+      });
+      player1.socket.join(roomName);
+      player2.socket.join(roomName);
+      io.to(player1.socket.id).emit('roomJoined', { rooms, roomName , playerColor:'red', playerCount:2});
+      io.to(player2.socket.id).emit('roomJoined', { rooms, roomName , playerColor:'blue', playerCount:2});
+      io.to(player1.socket.id).emit('playerJoined', {oppositePlayer:player2.nickName})
+      io.to(player2.socket.id).emit('playerJoined', {oppositePlayer:player1.nickName})
+      io.to(roomName).emit('startGame');
+    }
+  }
+});
+function checkWinner(grid, lastMoveCol) {
+  // Find the uppermost filled row in the specified column
+  let lastMoveRow;
+  for (let row = 0; row < 6; row++) {
+    if (grid[row][lastMoveCol]) {
+      lastMoveRow = row;
+      break;
+    }
+  }
+  // Check for a winner in horizontal direction
+  for (let col = Math.max(0, lastMoveCol - 3); col <= Math.min(3, lastMoveCol); col++) {
+    if (
+      grid[lastMoveRow][col] &&
+      grid[lastMoveRow][col] === grid[lastMoveRow][col + 1] &&
+      grid[lastMoveRow][col] === grid[lastMoveRow][col + 2] &&
+      grid[lastMoveRow][col] === grid[lastMoveRow][col + 3]
+    ) {
+      return grid[lastMoveRow][col]; // Winner found
+    }
+  }
+  // Check for a winner in vertical direction
+  for (let row = Math.max(0, lastMoveRow - 3); row <= Math.min(2, lastMoveRow); row++) {
+    if (
+      grid[row][lastMoveCol] &&
+      grid[row][lastMoveCol] === grid[row + 1][lastMoveCol] &&
+      grid[row][lastMoveCol] === grid[row + 2][lastMoveCol] &&
+      grid[row][lastMoveCol] === grid[row + 3][lastMoveCol]
+    ) {
+      return grid[row][lastMoveCol]; // Winner found
+    }
+  }
+  // Check for a winner in diagonal direction (top-left to bottom-right)
+  for (let offset = -3; offset <= 0; offset++) {
+    const row = lastMoveRow + offset;
+    const col = lastMoveCol + offset;
+    if (
+      row >= 0 && row + 3 < 6 &&
+      col >= 0 && col + 3 < 7 &&
+      grid[row][col] &&
+      grid[row][col] === grid[row + 1][col + 1] &&
+      grid[row][col] === grid[row + 2][col + 2] &&
+      grid[row][col] === grid[row + 3][col + 3]
+    ) {
+      return grid[row][col]; // Winner found
+    }
+  }
+  // Check for a winner in diagonal direction (top-right to bottom-left)
+  for (let offset = -3; offset <= 0; offset++) {
+    const row = lastMoveRow + offset;
+    const col = lastMoveCol - offset;
+    if (
+      row >= 0 && row + 3 < 6 &&
+      col - 3 >= 0 && col < 7 &&
+      grid[row][col] &&
+      grid[row][col] === grid[row + 1][col - 1] &&
+      grid[row][col] === grid[row + 2][col - 2] &&
+      grid[row][col] === grid[row + 3][col - 3]
+    ) {
+      return grid[row][col]; // Winner found
+    }
+  }
+  return null; // No winner yet
+}
+
+app.get("/", (req, res) => {
+  res.send("hello everyone");
+});
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ connect4frontend/src/App.js
@@ -0,0 +1,248 @@
+import React, { useState, useMemo, useEffect } from "react";
+import { io } from "socket.io-client";
+import "./App.css";
+const App = () => {
+  const socket = useMemo(
+    () =>
+      io("/", {
+        withCredentials: true,
+      }),
+    []
+  );
+  // const socket = useMemo(
+  //   () =>
+  //     io(process.env.REACT_APP_BACKEND, {
+  //       withCredentials: true,
+  //     }),
+  //   []
+  // );
+  const [roomName, setRoomName] = useState("");
+  const [password, setPassword] = useState("");
+  const [playerColor, setPlayerColor] = useState("");
+  const [showForm, setShowForm] = useState(true);
+  const [inLobby, setInLobby] = useState(false);
+  const [nickName, setNickName] = useState("");
+  const [oppositePlayer, setOppositePlayer] = useState("");
+  const [grid, setGrid] = useState(
+    Array.from({ length: 6 }, () => Array(7).fill(null))
+  );
+  const [turn, setTurn] = useState("red");
+  const [winner, setWinner] = useState(null);
+  const [started, setStarted] = useState(false);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      // console.log("user conected", socket.id);
+    });
+    socket.on("roomJoined",({ rooms, roomName, playerColor, playerCount, oppositePlayer }) => {
+        // console.log(`Joined room ${roomName}`, rooms, roomName, playerColor);
+        setShowForm(false);
+        setPlayerColor(playerColor);
+        setRoomName(roomName);
+      }
+    );
+    socket.on("invalidRoom", () => {
+      // console.log("Invalid room or password");
+      alert("Invalid Roomname or Password");
+      alert("Again ask your friend about it");
+    });
+    socket.on("playerJoined", ({ oppositePlayer }) => {
+      // console.log("Player joined", oppositePlayer);
+      setOppositePlayer(oppositePlayer);
+    });
+    socket.on("playerLeft", () => {
+      // console.log("Player left");
+      setStarted(false);
+    });
+    socket.on("startGame", () => {
+      // console.log("hello from game started");
+      setStarted(true);
+    });
+    socket.on("gameState", ({ newGrid, newTurn, newWinner }) => {
+      // console.log("hello from gamestate");
+      setGrid(newGrid);
+      setTurn(newTurn);
+      setWinner(newWinner);
+    });
+    return () => {
+      socket.disconnect();
+    };
+  }, [socket]);
+
+  const joinRoom = () => {
+    socket.emit("joinRoom", { roomName, password, nickName });
+    // console.log("hello after joinroom click and emit", socket.id, {roomName,password,});
+  };
+  const createRoom = () => {
+    socket.emit("createRoom", { roomName, password, nickName });
+    // console.log("hello after createroom click and emit", socket.id, {roomName,password,});
+    // console.log(showForm);
+  };
+  const handleClick = (col) => {
+    // console.log("hello from handle click function",roomName, playerColor, started);
+    if (winner || grid[0][col] !== null || turn !== playerColor || !started)
+      return;
+    const newGrid = [...grid];
+    for (let row = 5; row >= 0; row--) {
+      if (newGrid[row][col] === null) {
+        newGrid[row][col] = playerColor;
+        socket.emit("updateGame", {
+          roomName,
+          newGrid,
+          currentTurn: playerColor,
+          col,
+        });
+        break;
+      }
+    }
+  };
+  const enterLobby = () => {
+    setInLobby(true);
+    socket.emit("enterLobby", { nickName });
+  };
+  const leaveLobby = () => {
+    setInLobby(false);
+    socket.emit("leaveLobby");
+  };
+  const restartGame = () => {
+    // console.log("restart game clicked");
+    const newGrid = [...grid];
+    for (let col=0;col<=6;col++){
+      for (let row = 5; row >= 0; row--) {
+        if (newGrid[row][col] !== null) {
+          newGrid[row][col] = null;
+        }
+      }
+    }
+    socket.emit("restartGame", {
+      roomName,
+      newGrid,
+    });
+  };
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-purple-200 ">
+      <div className="bg-teal-100 p-8 rounded-lg shadow-md ">
+        <h1 className="text-3xl font-bold mb-4">Connect Four</h1>
+        {showForm ? (
+          <>
+            {inLobby ? (
+              <div>
+                <p className="mb-4">Waiting for a match...</p>
+                <button
+                  className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+                  onClick={leaveLobby}
+                >
+                  Leave Lobby
+                </button>
+              </div>
+            ) : (
+              <>
+                <form className="mb-4">
+                  <div className="mb-4">
+                    <label className="block text-sm font-semibold mb-2">
+                      Your Nick Name:
+                    </label>
+                    <input
+                      type="text"
+                      className="w-full border p-2 rounded-md"
+                      value={nickName}
+                      onChange={(e) => setNickName(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block text-sm font-semibold mb-2">
+                      Room Name:
+                    </label>
+                    <input
+                      type="text"
+                      className="w-full border p-2 rounded-md"
+                      value={roomName}
+                      onChange={(e) => setRoomName(e.target.value)}
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label className="block text-sm font-semibold mb-2">
+                      Password:
+                    </label>
+                    <input
+                      type="password"
+                      className="w-full border p-2 rounded-md"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </div>
+                  <button
+                    type="button"
+                    className="bg-blue-500 text-white px-4 py-2 rounded mr-2 hover:bg-blue-600"
+                    onClick={createRoom}
+                  >
+                    Create Room
+                  </button>
+                  <button
+                    type="button"
+                    className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                    onClick={joinRoom}
+                  >
+                    Join Room
+                  </button>
+                </form>
+                <h3 className="mb-2">Or Play with Online Players</h3>
+                <button
+                  className="bg-purple-500 text-white px-4 py-2 rounded hover:bg-purple-600"
+                  onClick={enterLobby}
+                >
+                  Play Online
+                </button>
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            {winner ? (
+              <>
+                {winner === playerColor ? (
+                  <>
+                    <p className="mb-4">Yay, you win!</p>
+                  </>
+                ) : (
+                  <>
+                    <p className="mb-2">Oops, better luck next time 😅</p>
+                  </>
+                )}
+                <button
+                  className="bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600 m-3"
+                  onClick={restartGame}
+                >
+                  Restart Game
+                </button>
+              </>
+            ) : (
+              <>
+                {turn === playerColor ? (
+                  <p className="mb-3">Make a move</p>
+                ) : (
+                  <p className="mb-3">Wait for your turn</p>
+                )}
+                <p className="mb-3">{`${nickName} vs ${oppositePlayer}`}</p>
+              </>
+            )}
+            <div className="grid">
+              {grid.map((row, rowIndex) => (
+                <div key={rowIndex} className="row">
+                  {row.map((cell, colIndex) => (
+                    <div
+                      key={colIndex}
+                      className={`cell ${cell}`}
+                      onClick={() => handleClick(colIndex)}
+                    ></div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+export default App;
